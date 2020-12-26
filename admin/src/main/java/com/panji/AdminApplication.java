98291c229f1a6a59352c47788af30c6459a3a08e
guang <guang@xingren.com>
2020-12-26 17:02:49 +0800

    build: 添加自定义 banner

--- admin/src/main/java/com/panji/AdminApplication.java
+++ admin/src/main/java/com/panji/AdminApplication.java
@@ -3,10 +3,7 @@ package com.panji;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author Leo
- * @date 10:37 2019/8/26
- **/
+
 @SpringBootApplication
 public class AdminApplication {
 
